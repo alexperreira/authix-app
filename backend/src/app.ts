a01aaa5027f76a801d468d1d1689e240f7005ee7
alexperreira <kali@kali>
2025-05-30 12:27:08 -0400

    added dotenv.config() to app.ts so DATABASE_URL could be found in app.ts / Final insecure version before implementing security fixes.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
